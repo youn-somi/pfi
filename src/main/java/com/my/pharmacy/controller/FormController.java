1fb623e9ba1424f943b10ed7b5be8463613cbdf1
youn-somi <thaldlqslek@naver.com>
2026-05-26 14:51:53 +0900

    feat : 주소로 검색 하기 서비스 구성완료

--- src/main/java/com/my/pharmacy/controller/FormController.java
+++ src/main/java/com/my/pharmacy/controller/FormController.java
@@ -1,8 +1,13 @@
 package com.my.pharmacy.controller;
 
+import com.my.pharmacy.dto.DocumentDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
 public class FormController {
 
@@ -15,4 +20,16 @@ public class FormController {
     public String outputForm() {
         return "output";
  }
+@PostMapping("/search")
+    public  String searchAddress(@RequestParam("address")String address) {
+        log.info("결과 : " + kakaoApiResponseDto);
+
+        //결과 중 Documents  만 빼서 dto 저장
+    DocumentDto documentDto = kakaoApiResponseDto
+            .getDocumentList().get(0);
+    log.info("도큐먼트만 출력 : " + documentDto);
+        return "output";
+
+}
+
 }
